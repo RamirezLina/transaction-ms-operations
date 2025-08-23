3b43eab0ca3c0e61d23986ae47579dc6eae783a6
LinaRamirez <lina.ramirez@sofka.com.co>
2025-08-23 16:53:45 -0500

    Start project

---
+++ src/main/java/com/transactionmgmt/operations/MsOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.transactionmgmt.operations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsOperationsApplication.class, args);
+	}
+
+}
