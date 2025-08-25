39af8b2a8d7e3b53289bcaee9f03e44140fd443c
LinaRamirez <lina.ramirez@sofka.com.co>
2025-08-24 22:25:37 -0500

    Refactor mappers

--- src/main/java/com/transactionmgmt/operations/persistence/mappers/MovementEntityMapper.java
+++ src/main/java/com/transactionmgmt/operations/persistence/mappers/MovementEntityMapper.java
@@ -3,12 +3,10 @@ package com.transactionmgmt.operations.persistence.mappers;
 import com.transactionmgmt.operations.domain.movement.Movement;
 import com.transactionmgmt.operations.persistence.entities.MovementEntity;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface MovementEntityMapper {
-    MovementEntityMapper INSTANCE = Mappers.getMapper(MovementEntityMapper.class);
 
     MovementEntity toEntity(Movement movement);
-    Movement toDomain(MovementEntity entity);
+    Movement toModel(MovementEntity entity);
 }
